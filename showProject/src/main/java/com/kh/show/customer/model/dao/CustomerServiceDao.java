79d03d89abaedf53b83521dde49d38044c41f745
Jeon sumin <sumin1236@naver.com>
2024-12-20 11:06:12 +0900

    2024-12-19 작업 커밋

--- showProject/src/main/java/com/kh/show/customer/model/dao/CustomerServiceDao.java
+++ showProject/src/main/java/com/kh/show/customer/model/dao/CustomerServiceDao.java
@@ -6,17 +6,45 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.show.customer.model.vo.Faq;
+import com.kh.show.customer.model.vo.Question;
+import com.kh.show.customer.model.vo.Reservation;
 
 @Repository
 public class CustomerServiceDao {
 
+	// faq 리스트 
 	public ArrayList<Faq> selectFaqList(SqlSessionTemplate sqlSession) {
-		return (ArrayList)sqlSession.selectList("custopperMapper.selectFaqList");
+		return (ArrayList)sqlSession.selectList("customerMapper.selectFaqList");
 	}
 
+	// faq 카테고리별 리스트 
 	public ArrayList<Faq> faqFilterList(SqlSessionTemplate sqlSession, String qcategoryNo) {
 
-		return (ArrayList)sqlSession.selectList("custopperMapper.faqFilterList", qcategoryNo);
+		return (ArrayList)sqlSession.selectList("customerMapper.faqFilterList", qcategoryNo);
+	}
+
+	// faq 키워드 검색 리스트
+	public ArrayList<Faq> faqSearchList(SqlSessionTemplate sqlSession, String keyword) {
+
+		return (ArrayList)sqlSession.selectList("customerMapper.faqSearchList",keyword);
+	}
+
+	// 1:1 문의 저장 메소드 
+	public int questionInsert(SqlSessionTemplate sqlSession, Question q) {
+		return sqlSession.insert("customerMapper.questionInsert", q);
+	}
+
+	// 1:1 문의 저장 메소드 - 비회원
+	public int questionNonInsert(SqlSessionTemplate sqlSession, Question q) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("customerMapper.questionNonInsert", q);
+	}
+
+	// 회원 예약 번호 출력 메소드 
+		// Reservation import 다시하기 mapper도 reservation으로 이동?  
+	public ArrayList<Reservation> reSearch(SqlSessionTemplate sqlSession, int userNo) {
+		System.out.println(userNo);
+		return (ArrayList)sqlSession.selectList("customerMapper.reSearch", userNo);
 	}
 
 }
